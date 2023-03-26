150e8f00672a66865514fe8bdbf26854f87ab4a9
Kurniaman Andreas Zega <nimanz720@gmail.com>
2023-03-26 14:49:58 +0700

    update registration: adding upload and service

--- app/Http/Livewire/Pages/Registration.php
+++ app/Http/Livewire/Pages/Registration.php
@@ -1,22 +1,38 @@
 <?php
 
 namespace App\Http\Livewire\Pages;
-
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Registration extends Component
 {
+    //name, email, password
     public $name;
     public$email;
     public$password;
+
     public function render()
     {
         return view('livewire.pages.registration');
     }
 
+    //file uploads
+    use WithFileUploads;
+    public$photo;
     public function submit()
     {
+        dd($this->name,$this->email,$this->password,$this->photo,$this->service
 
+        );
+        $this->validate([
+            'photo' => 'image|max:1024', // besarnya <= 1 MB
+        ]);
+
+        $this-> photo -> store('photos');
     }
+
+    //checkbox
+    public $service= ['website'];
+
 
 }
--- resources/views/livewire/pages/registration.blade.php
+++ resources/views/livewire/pages/registration.blade.php
@@ -1,10 +1,10 @@
-<div class="h-screen w-full pt-16 px-10 bg-slate-50">
+<div class="h-full w-full py-14 px-10 bg-slate-50">
     <div>
-        <div class="flex flex-row h-[820px]">
+        <div class="flex flex-row h-[1080px] drop-shadow-lg">
             <div class="w-1/2">
                 <img class="object-cover h-full" src="https://images.unsplash.com/photo-1608501078713-8e445a709b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="">
             </div>
-            <div class=" w-1/2 px-[104px] bg-white drop-shadow-lg pt-[96px]">
+            <div class=" w-1/2 px-[104px] bg-white pt-[96px]">
                 <div>
                     <h1 class="text-black font-semibold text-[48px]">Let's level up your brand, <br>together</h1>
                 <p class="text-[#667085] mt-[24px]">You can reach us anytime via
@@ -30,8 +30,26 @@
                             </p>
                             <input class="border-solid border-2 rounded-md w-full mt-[6px] font-normal px-[12px] py-[16px]" placeholder="Your password" type="password" name="password" wire:model="password">
 
-                            <div class="flex flex-col items-center justify-center">
-                                <button class="bg-[#7F56D9] w-full h-[48px] text-white rounded-md text-[16px] font-medium mt-[32px]" type="submit">Register</button>
+                            <p class="font-medium text-[#344054] text-[14px] mt-[24px]">
+                                Upload File
+                            </p>
+                            <input class="mt-[6px]" type="file" wire:model="photo">
+
+                            <p class="font-medium text-[#344054] text-[14px] mt-[24px]">
+                                Services
+                            </p>
+                            <div class="flex  items-center mt-[16px]">
+                                <input class="w-[20px] h-[20px] rounded-md border-[#D0D5DD] " type="checkbox" value="website" wire:model="service">
+                                <label class="ml-2 font-medium text-[16px] ">Website design</label>
+                            </div>
+
+                            <div class="flex  items-center mt-[16px]">
+                                <input class="w-[20px] h-[20px] rounded-md border-[#D0D5DD] "  type="checkbox" value="uxdesign" wire:model="service">
+                                <label class="ml-2 font-medium text-[16px] ">UX Design</label>
+
+                            </div>
+                            <div class="flex flex-col items-center justify-center mt-[12px]">
+                                <button class="bg-[#7F56D9] w-full h-[48px] text-white rounded-lg text-[16px] font-medium mt-[32px]" type="submit">Register</button>
                             </div>
 
                         </div>
